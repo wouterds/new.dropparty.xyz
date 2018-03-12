a3ad0a77cb5007099cf7cbab21bb36bb0ade4de8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-12 23:03:52 +0100

    :zap: Some extras for express web server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import path from 'path';
 import express from 'express';
+import compression from 'compression';
+import bodyParser from 'body-parser';
 import ServeStatic from 'serve-static';
 import AppLoader from './app-loader';
 
 const app = express();
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(ServeStatic(path.join(__dirname, './../public')));
 app.use(AppLoader);
