0a5c19834743bf93e080731ef7a4478b0c3b5916
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-12 01:23:09 +0100

    :bug: Fixed CSS for SSR

--- webpack.config.ssr.babel.js
+++ webpack.config.ssr.babel.js
@@ -3,6 +3,9 @@ import config from './webpack.config.babel';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import NodeExternals from 'webpack-node-externals';
 import FlowBabelWebpackPlugin from 'flow-babel-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const production = process.env.NODE_ENV === 'production';
 
 config.target = 'node';
 config.externals = [NodeExternals()];
@@ -16,6 +19,30 @@ config.plugins = config.plugins.filter((plugin) => {
     plugin instanceof HtmlWebpackPlugin ||
     plugin instanceof FlowBabelWebpackPlugin
   );
+});
+config.plugins.push(new ExtractTextPlugin({ filename: '[name].css', allChunks: true }));
+config.module.rules = config.module.rules.filter((rule) => {
+  return !(
+    rule.test === /\.css$/
+  );
+});
+config.module.rules.push({
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 1,
+          localIdentName: production ? '[hash:base64]' : '[name]-[local]-[hash:8]',
+          sourceMap: !production,
+        },
+      },
+      'postcss-loader',
+    ],
+  }),
 });
 
 module.exports = config;
