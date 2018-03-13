3f9fee40b6d25cf24fce677efeedcf964704f244
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-13 01:09:48 +0100

    :sparkles: Header with signed in user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,19 +2,53 @@
 import React, { Component } from 'react';
 import type { Node } from 'react';
 import styles from './styles.css';
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
 
-class Header extends Component<{}>
+type State = {
+  user: ?Object,
+};
+
+class Header extends Component<{}, State>
 {
+  constructor() {
+    super(...arguments);
+
+    let user = null;
+    if (Cookies.get('token')) {
+      const decodedToken = jwtDecode(Cookies.get('token'));
+
+      user = {
+        first_name: decodedToken.first_name,
+        name: decodedToken.name,
+        email: decodedToken.email,
+      };
+    }
+
+    this.state = {
+      user,
+    };
+  }
   /**
    * Render the component
    *
    * @returns {Node}
    */
   render(): Node {
+    const { user } = this.state;
+
     return (
       <header className={styles.header}>
-        <h1>Drop Party</h1>
-        <hr />
+        <div className={styles.left}>
+            <h1>Drop Party</h1>
+            <hr />
+        </div>
+        {user ? (
+          <div className={styles.right}>
+            <div className={styles.name}>{user.first_name} {user.name}</div>
+            <div className={styles.email}>{user.email}</div>
+          </div>
+        ) : null}
       </header>
     );
   }
