b527ac294fbdb5656fcf97bf0813f01c877481c9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-13 23:58:08 +0100

    :lipstick: Menu button that doesn't do anything

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -48,8 +48,13 @@ class Header extends Component<{}, State>
         </div>
         {user ? (
           <div className={styles.right}>
-            <div className={styles.name}>{user.first_name} {user.name}</div>
-            <div className={styles.email}>{user.email}</div>
+            <div className={styles.info}>
+              <div className={styles.name}>{user.first_name} {user.name}</div>
+              <div className={styles.email}>{user.email}</div>
+            </div>
+            <button className={styles.buttonMore}>
+              <i className="material-icons">more_horiz</i>
+            </button>
           </div>
         ) : null}
       </header>
