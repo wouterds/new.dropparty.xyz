0c047fea933b124f20f302d3b13dfa278818a2ec
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-14 00:14:32 +0100

    :sparkles: Invite to code

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -15,7 +15,12 @@ class Footer extends Component<{}>
   render(): Node {
     return (
       <footer className={styles.footer}>
-        &copy; {new Date().getFullYear()} <a href="https://dropparty.xyz/">dropparty.xyz</a>
+        <div className={styles.left}>
+          &copy; {new Date().getFullYear()} <a href="https://dropparty.xyz/">dropparty.xyz</a>
+        </div>
+        <div className={styles.right}>
+          Can you code, like to contribute? Check out the <a href="https://github.com/wouterds/dropparty.xyz/">website</a> &amp; <a href="https://github.com/wouterds/api.dropparty.xyz/">API</a> on Github!
+        </div>
       </footer>
     );
   }
