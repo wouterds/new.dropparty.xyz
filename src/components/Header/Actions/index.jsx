7852bc77b77aec48e3307c9814e4960a384e8076
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-14 01:16:40 +0100

    :construction: Disabled unused links for now

--- src/components/Header/Actions/index.jsx
+++ src/components/Header/Actions/index.jsx
@@ -24,10 +24,10 @@ class Header extends Component<Props>
 
     return (
       <ul className={styles.actions}>
-        <li><Link to='/files'>My Files</Link></li>
-        <li><Link to='/stats'>Stats</Link></li>
-        <li><Link to='/referrals'>Referrals</Link></li>
-        <li><Link to='/settings'>Settings</Link></li>
+        {/* <li><Link to='/files'>My Files</Link></li> */}
+        {/* <li><Link to='/stats'>Stats</Link></li> */}
+        {/* <li><Link to='/referrals'>Referrals</Link></li> */}
+        {/* <li><Link to='/settings'>Settings</Link></li> */}
         <li><Link to='/sign-out'>Sign Out</Link></li>
       </ul>
     );
