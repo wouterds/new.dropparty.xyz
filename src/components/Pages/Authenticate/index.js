ecb7256df0e23e74989225ef4bc3e6c90cdfddb4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-17 00:02:29 +0100

    :sparkles: Show sign in key

--- src/components/Pages/Authenticate/index.js
+++ src/components/Pages/Authenticate/index.js
@@ -1,6 +1,7 @@
 //@flow
 import React, { Component } from 'react';
 import type { Node } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.css';
 import Footer from 'components/Footer';
 import Base from 'components/Pages/Base';
@@ -12,13 +13,32 @@ type Props = {
   history: Object,
 };
 
-class Authenticate extends Component<Props>
+type State = {
+  token: ?string,
+};
+
+class Authenticate extends Component<Props, State>
 {
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      token: null,
+    };
+  }
   /**
    * When the component mounted
    */
   componentDidMount() {
     const token = getUrlParam('token');
+
+    let show = !!getUrlParam('show');
+
+    if (!show) {
+      show = Cookies.get('show');
+    }
+
+    Cookies.set('show', show);
 
     // Empty token?
     if (!token) {
@@ -27,6 +47,12 @@ class Authenticate extends Component<Props>
     }
 
     Cookies.set('token', token, { expires: 30 });
+
+    if (show) {
+      this.setState({ token });
+      Cookies.remove('show');
+      return;
+    }
 
     this.props.history.push('/');
   }
@@ -37,9 +63,28 @@ class Authenticate extends Component<Props>
    * @returns {Node}
    */
   render(): Node {
+    const { token } = this.state;
+
     return (
       <div className={styles.authenticate}>
-        <p>Signing you in..</p>
+        {token ? (
+          <div>
+            <h2>Sign in</h2>
+            <p>Copy your sign in key below and paste it in the app to sign in.</p>
+
+            <br />
+            <textarea readonly rows='8'>{token}</textarea>
+
+            <br />
+
+            <Link to='/' className={styles.button}>Back</Link>
+          </div>
+        ) : (
+          <div>
+            <h2>Sign in</h2>
+            <p>Redirecting you to Dropbox..</p>
+          </div>
+        )}
       </div>
     );
   }
